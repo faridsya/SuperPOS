a82036b9078a06fed60d9d2c2bd7cb6fd4b6f9d8
FaridPC <farid0910@gmail.com>
2023-04-29 11:22:45 +0700

    Kantor

--- app/src/main/java/com/app/superpos/SplashActivity.java
+++ app/src/main/java/com/app/superpos/SplashActivity.java
@@ -3,7 +3,9 @@ package com.app.superpos;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
@@ -13,16 +15,23 @@ public class SplashActivity extends AppCompatActivity {
 
 
     public static int splashTimeOut = 1000;
-
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sp = getSharedPreferences(Constant.SHARED_PREF_NAME, Context.MODE_PRIVATE);
+        String email = sp.getString(Constant.SP_EMAIL, "");
+        String shopName = sp.getString(Constant.SP_SHOP_NAME, "");
+
         setContentView(R.layout.activity_splash);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
         new Handler().postDelayed(() -> {
-            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            Intent intent = new Intent(SplashActivity.this,  LoginActivity.class);
+            if (email.length() >= 3 && shopName.length()>=3 ) {
+                intent = new Intent(SplashActivity.this,  HomeActivity.class);
+            }
             startActivity(intent);
             finish();
         }, splashTimeOut);
